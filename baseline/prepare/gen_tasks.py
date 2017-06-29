82e2bdc4b096f73c58c2a90da07a8bb28dcf6188
Aleksandr Panov <panov.ai@ya.ru>
2017-06-29 21:27:49 +0300

    Fix generation

--- baseline/prepare/gen_tasks.py
+++ baseline/prepare/gen_tasks.py
@@ -1,17 +1,117 @@
 #!/usr/bin/env python
 
 import argparse
+import cPickle
+import collections
+import hashlib
 import os
 import random
 
-from dlpf.gym_environ.search_algo import EuclideanAStar
-from dlpf.utils.base_utils import init_log, LOGGING_LEVELS
-from dlpf.utils.task_utils import load_map_from_compact, PathFindingTask, save_to_compact
+import numpy
+from scipy.spatial.distance import euclidean
+
+from pathenv.tasks import BY_PIXEL_ACTION_DIFFS
+
+PathFindingTask = collections.namedtuple('PathFindingTask',
+                                         'title local_map start finish path'.split(' '))
+
+CompactPathFindingTask = collections.namedtuple('CompactPathFindingTask',
+                                                'map_id start finish path'.split(' '))
+
+StepResult = collections.namedtuple('StepResult',
+                                    'must_continue best_next new_variants_with_ratings'.split(' '))
+
+
+class BaseSearchAlgo(object):
+    def __init__(self):
+        pass
+
+    def reset(self, local_map, start, finish):
+        self.local_map = local_map
+        self.start = start
+        self.finish = finish
+
+        self.queue = [self.start]
+        self.ratings = {self.start: 0}
+        self.backrefs = {self.start: self.start}
+        self.visited_nodes = set()
+
+    def walk_to_finish(self):
+        while self.step().must_continue:
+            pass
+
+    def step(self):
+        if self.goal_achieved():
+            return StepResult(False, self.finish, [])
+
+        while len(self.queue) > 0:
+            self._reorder_queue()
+            best_next = self.queue.pop()
+            self.visited_nodes.add(best_next)
+
+            if self.goal_achieved():
+                return StepResult(False, self.finish, [])
+
+            new_variants_with_ratings = self._gen_new_variants(best_next)
+            if len(new_variants_with_ratings) == 0:
+                continue
+
+            self.queue.extend(p for p, _ in new_variants_with_ratings)
+            self.ratings.update(new_variants_with_ratings)
+            self.backrefs.update((new_point, best_next) for new_point, _ in new_variants_with_ratings)
+            return StepResult(True, best_next, new_variants_with_ratings)
+
+        return StepResult(False, None, [])
+
+    def update_ratings(self, updates):
+        self.ratings.update(updates)
+
+    def goal_achieved(self):
+        return self.finish in self.visited_nodes
+
+    def get_best_path(self):
+        self.walk_to_finish()
+        if not self.goal_achieved():
+            return None
+
+        result = [self.finish]
+        while result[-1] != self.start:
+            result.append(self.backrefs[result[-1]])
+        result.reverse()
+        return result
+
+    def _reorder_queue(self):
+        self.queue.sort(key=self.ratings.__getitem__)
+
+    def _gen_new_variants(self, pos):
+        y, x = pos
+        all_new_points = ((y + dy, x + dx)
+                          for dy, dx
+                          in BY_PIXEL_ACTION_DIFFS.viewvalues())
+        return [(point, -euclidean(point, self.finish))
+                for point in all_new_points
+                if (not point in self.backrefs)
+                and (0 <= point[0] < self.local_map.shape[0])
+                and (0 <= point[1] < self.local_map.shape[1])
+                and (self.local_map[point] == 0)]
 
 
 def _gen_point(map_shape):
     return (random.randint(0, map_shape[0] - 1),  # randint is inclusive
             random.randint(0, map_shape[1] - 1))
+
+
+def save_to_compact(task, maps_dir, paths_dir):
+    map_id = hashlib.md5(local_map.tostring()).hexdigest()
+    map_fname = os.path.join(maps_dir, map_id)
+    if not os.path.exists(map_fname + '.npz'):
+        numpy.savez_compressed(map_fname, task.local_map)
+
+    compact = CompactPathFindingTask(map_id, task.start, task.finish, task.path)
+    task_fname = os.path.join(paths_dir, task.title + '.pickle')
+
+    with open(task_fname, 'wb') as f:
+        cPickle.dump(compact, f, 2)
 
 
 if __name__ == '__main__':
@@ -29,18 +129,18 @@ if __name__ == '__main__':
 
     args = aparser.parse_args()
 
-    logger = init_log(stderr=True)
-
-    path_builder = EuclideanAStar()
+    path_builder = BaseSearchAlgo()
 
     map_dir = os.path.dirname(args.map_fname)
-    local_map = load_map_from_compact(args.map_fname)
+    with numpy.load(args.map_fname) as f:
+        local_map = f['arr_0']
     for i in xrange(args.n):
         while True:
             start = _gen_point(local_map.shape)
             finish = _gen_point(local_map.shape)
             if local_map[start] != 0 or local_map[finish] != 0:
                 continue
+
             path_builder.reset(local_map, start, finish)
             path = path_builder.get_best_path()
             if not path is None:
--- baseline/prepare/import.py
+++ baseline/prepare/import.py
@@ -1,10 +1,66 @@
 #!/usr/bin/env python
 
 import argparse
+import cPickle
+import collections
+import hashlib
 import os
 
-from dlpf.utils.base_utils import init_log, LOGGING_LEVELS
-from dlpf.utils.task_utils import import_tasks_from_xml_to_compact
+import lxml
+import numpy
+
+PathFindingTask = collections.namedtuple('PathFindingTask',
+                                         'title local_map start finish path'.split(' '))
+
+CompactPathFindingTask = collections.namedtuple('CompactPathFindingTask',
+                                                'map_id start finish path'.split(' '))
+
+
+def load_from_xml(fname, ctor=PathFindingTask):
+    try:
+
+        with open(fname, 'r') as f:
+            tree = lxml.etree.parse(f)
+
+        title = os.path.splitext(os.path.basename(fname))[0]
+
+        local_map = numpy.array([map(int, row.split(' '))
+                                 for row in tree.xpath('/root/map/grid/row/text()')],
+                                dtype='uint8')
+        start_x = int(tree.xpath('/root/map/startx/text()')[0])
+        start_y = int(tree.xpath('/root/map/starty/text()')[0])
+        finish_x = int(tree.xpath('/root/map/finishx/text()')[0])
+        finish_y = int(tree.xpath('/root/map/finishy/text()')[0])
+
+        sections = tree.xpath('/root/log[1]/hplevel/section')
+        sections.sort(key=lambda n: int(n.get('number')))
+        path = [(int(s.get('start.y')),
+                 int(s.get('start.x')))
+                for s in sections]
+        if path:
+            path.append((int(sections[-1].get('finish.y')),
+                         int(sections[-1].get('finish.x'))))
+        result = ctor(title,
+                      local_map,
+                      (start_y, start_x),
+                      (finish_y, finish_x),
+                      path)
+        return result
+    except:
+        return None
+
+
+def save_to_compact(task, maps_dir, paths_dir):
+    map_id = hashlib.md5(task.local_map.tostring()).hexdigest()
+    map_fname = os.path.join(maps_dir, map_id)
+    if not os.path.exists(map_fname + '.npz'):
+        numpy.savez_compressed(map_fname, task.local_map)
+
+    compact = CompactPathFindingTask(map_id, task.start, task.finish, task.path)
+    task_fname = os.path.join(paths_dir, task.title + '.pickle')
+    with open(task_fname, 'wb') as f:
+        cPickle.dump(compact, f, 2)
+
 
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser()
@@ -12,7 +68,14 @@ if __name__ == "__main__":
     aparser.add_argument('target_dir', type=str)
 
     args = aparser.parse_args()
+    maps_subdir = 'maps'
+    paths_subdir = 'paths'
 
-    logger = init_log(stderr=True)
+    maps_dir = os.path.join(args.target_dir, maps_subdir)
+    paths_dir = os.path.join(args.target_dir, paths_subdir)
 
-    import_tasks_from_xml_to_compact(args.src_dir, args.target_dir)
+    for in_fname in os.listdir(args.src_dir):
+        task = load_from_xml(os.path.join(args.src_dir, in_fname))
+        if task is None:
+            continue
+        save_to_compact(task, maps_dir, paths_dir)
